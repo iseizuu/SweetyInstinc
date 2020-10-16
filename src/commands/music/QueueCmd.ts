7b6ac7df0c2795186947b8a7c807e17bba4b36e7
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-10-17 00:51:17 +0700

    feat: add queue command

---
+++ src/commands/music/QueueCmd.ts
@@ -0,0 +1,79 @@
+import { stripIndent } from 'common-tags';
+import { Message, MessageEmbed, MessageReaction, User } from 'discord.js';
+import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+import Client from '../../structures/Client';
+
+export default class QueueCommand extends Command {
+    constructor(client: CommandoClient) {
+        super(client, {
+            name: 'queue',
+            aliases: ['q', 'musiclist'],
+            group: 'music',
+            memberName: 'queuecmd',
+            description: 'Show all queue tracks list',
+            guildOnly: true,
+            throttling: {
+                usages: 1,
+                duration: 5,
+            },
+        });
+    }
+    public async run(msg: CommandoMessage): Promise<Message | Message[]> {
+        const player = await (this.client as Client).lava.songs.get(msg.guild.id);
+        if (!player) return msg.say('**There is no song playing right now!**');
+        const queue = player.queue.tracks
+            .map((x: { info: { title: string; uri: string; }; }, y: number) => stripIndent`
+            **${y + 1} - [${x.info.title}](${x.info.uri})**`);
+        if (!queue.length) {
+            return msg.embed({
+                title: `${(this.client as Client).config.emojis.no} There is no queue`,
+                color: (this.client as Client).config.color,
+                description: `**Current Track [${player.queue.current.info.title}](${player.queue.current.info.uri}) [${player.queue.current.duration}]**`,
+            });
+        }
+        const list = this.arr(queue, 10);
+        const emoji = ['◀', '▶'];
+        let index = 0;
+        const embed = new MessageEmbed()
+            .setTitle(`🎵 Queue - ${queue.length} Songs`)
+            .setColor((this.client as Client).config.color)
+            .setDescription(list[index].join('\n'))
+            .setFooter(`Page: ${index + 1}/${list.length}`);
+        const msgg = await msg.channel.send(embed);
+        if (list.length < 10) return;
+        for (const r of emoji) {
+            await msgg.react(r);
+        }
+        const filter = (reaction: MessageReaction, user: User) => {
+            return reaction.emoji.name === emoji[0] || reaction.emoji.name === emoji[1] && user.id === msg.author.id;
+        };
+        const react = await msgg.createReactionCollector(filter, {
+            time: 3e4,
+        });
+
+        react.on('collect', (e: MessageReaction) => {
+            const collect = e.emoji.name;
+            if (collect === emoji[0]) index--;
+            if (collect === emoji[1]) index++;
+            index = ((index % list.length) + list.length) % list.length;
+            msgg.edit({
+                embed: {
+                    title: `🎵 Queue - ${queue.length} Songs`,
+                    color: (this.client as Client).config.color,
+                    description: list[index].join('\n'),
+                    footer: {
+                        text: `Page: ${index + 1}/${list.length}`,
+                    },
+                },
+            });
+        });
+    }
+
+    public arr(array: Array<string>, amount: number) {
+        const arr = [];
+        for (let i = 0; i < array.length; i += amount) {
+            arr.push(array.slice(i, i + amount));
+        }
+        return arr;
+    }
+}
